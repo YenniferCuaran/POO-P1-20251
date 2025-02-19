bb8128bc04eb69f0b1df47823f547c1ac81cd10d
YenniferCuaran <yenniferl.cuarani@uqvirtual.edu.co>
2025-02-19 10:46:15 -0500

    update
    nuevos proyectos por orden los cree

---
+++ Ejercicios/src/main/java/co/edu/uniquindio/poo/Ejercicio1.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.poo;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println("Hola Yenny");
+    }
+}
