3917295163a742aedad3f363ea0a9fd90e35cb8c
YenniferCuaran <yenniferl.cuarani@uqvirtual.edu.co>
2025-02-19 18:12:53 -0500

    update

--- Actividades/src/main/java/co/edu/uniquindio/poo/Nino.java
+++ Actividades/src/main/java/co/edu/uniquindio/poo/Nino.java
@@ -1,4 +1,18 @@
 package co.edu.uniquindio.poo;
 
+
+//CASO ESTUDIO: Guarderia
 public class Nino {
+    public static void main(String[]args){
+
+        String nombre;
+        String docIdentidad;
+        String alergias;
+        String nombreAcudiente;
+        String numeroContacto;
+        int edad;
+        char genero;
+
+
+    }
 }
--- Tareas/src/main/java/co/edu/uniquindio/poo/Estudiante.java
+++ Tareas/src/main/java/co/edu/uniquindio/poo/Estudiante.java
@@ -1,8 +1,28 @@
 package co.edu.uniquindio.poo;
 
 public class Estudiante {
-    public static void main(String[] args) {
-        System.out.println("Estudiante lis");
+
+    private String nombres;
+    private String apellidos;
+    private String correo;
+    private int edad;
+    private int telefono;
+    private int identificacion;
+
+    public Estudiante(String nombres, String apellidos, String correo, int edad, int telefono, int identificacion) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.edad = edad;
+        this.telefono = telefono;
+        this.identificacion = identificacion;
     }
 
+    @Override
+    public String toString() {
+        return "Estudiante{" +
+                "nombre ='" + nombres + '\'' +
+                "edad ='" + edad + '\'' +
+                '}';
+    }
 }
