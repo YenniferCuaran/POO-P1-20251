daeb831340898abd0763e765001403ed5994ef2d
YenniferCuaran <yenniferl.cuarani@uqvirtual.edu.co>
2025-02-19 14:51:47 -0500

    project

--- Ejercicios/src/main/java/co/edu/uniquindio/poo/Ejercicio1.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uniquindio.poo;
-
-import org.w3c.dom.ls.LSOutput;
-
-public class Ejercicio1 {
-    public static void main(String[] args) {
-        System.out.println("Hola Yenny");
-    }
-}
