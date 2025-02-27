c6da0ab9fca8a49922ab6b85ba3bec27cf02197b
YenniferCuaran <yenniferl.cuarani@uqvirtual.edu.co>
2025-02-27 10:03:52 -0500

    update

---
+++ ejercicios/src/main/java/co/edu/uniquindio/poo/Ejercicio1.java
@@ -0,0 +1,17 @@
+/* Imprimir los primeros 10 números naturales
+Escribe un programa que use un bucle for para imprimir los números del 1 al 10.
+
+ */
+
+package co.edu.uniquindio.poo;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
---
+++ ejercicios/src/main/java/co/edu/uniquindio/poo/Ejercicio2.java
@@ -0,0 +1,10 @@
+
+/* Sumar los primeros N números naturales
+Escribe un programa que pida un número N al usuario y calcule la suma de los primeros N números naturales usando un bucle while.
+      */
+        package co.edu.uniquindio.poo;
+
+
+public class Ejercicio2 {
+
+}
