1fa2f5f13f8a858aeec3278d4c38b9f0a6bb81e6
YenniferCuaran <yenniferl.cuarani@uqvirtual.edu.co>
2025-03-04 12:44:52 -0500

    update

--- ejercicios/src/main/java/co/edu/uniquindio/poo/Ejercicio2.java
+++ ejercicios/src/main/java/co/edu/uniquindio/poo/Ejercicio2.java
@@ -1,10 +1,22 @@
 
 /* Sumar los primeros N números naturales
-Escribe un programa que pida un número N al usuario y calcule la suma de los primeros N números naturales usando un bucle while.
+Escribe un programa que pida un número N al usuario y calcule
+ la suma de los primeros N números naturales usando un bucle while.
       */
-        package co.edu.uniquindio.poo;
+package co.edu.uniquindio.poo;
 
+
+import java.util.Scanner;
 
 public class Ejercicio2 {
+        public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.println("Ingrese un numero");
+                int numero = scanner.nextInt();
+
+                while (numero)
+
+        }
 
 }
